c43237df3432b7940156dc0de5a5963bd343fe04
Egor Chupin <egusator@gmail.com>
2023-12-21 00:06:24 +0700

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Monitor monitor = new Monitor(false);
-        while (true) {
+        for (int i = 0; i < 100000; i++) {
             ProviderThread providerThread = new ProviderThread();
             providerThread.setMonitor(monitor);
             providerThread.start();
